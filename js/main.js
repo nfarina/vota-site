a8c99758690ddc237112d70a4efa50761d118d4b
Josh Holloran <>
2018-05-10 14:58:41 -0700

    hero

---
+++ js/main.js
@@ -0,0 +1,29 @@
+---
+---
+
+  // FORM INPUT BEHAVIOR
+
+	var input = document.querySelector("input[type='tel']");
+
+  if (input && input.value) {
+    input.classList.add('filled');
+  } // for Firefox that keeps form input values on reload
+
+  input.addEventListener('blur', function() {
+    if (input && input.value) {
+      input.classList.add('filled');
+    }
+    else {
+      input.classList.remove('filled');
+    }
+  });
+
+  // CLEAR ERROR STATE WHEN CLICKING INPUTS
+
+  input.addEventListener('click', function() {
+      input.classList.remove('error');
+  });
+
+  input.addEventListener('keyup', function() {
+      input.classList.remove('error');
+  });
